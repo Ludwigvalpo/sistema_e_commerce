6677db19d9b6f922418bf246436a3db726ff0542
Ramon Bernabe Muñoz Navarrete <rmkmegod@gmail.com>
2022-07-15 20:28:30 -0400

    Agrega scaffold de size

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :sizes
   resources :colors
   get 'home/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_001840) do
+ActiveRecord::Schema.define(version: 2022_07_16_002710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,12 @@ ActiveRecord::Schema.define(version: 2022_07_16_001840) do
     t.integer "stock", default: 0
     t.decimal "price"
     t.string "sku"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sizes", force: :cascade do |t|
+    t.string "label", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
